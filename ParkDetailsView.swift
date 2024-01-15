eb6bde663d606832e07134f02f265ecf6b4b5fc8
DanielPuente <puente10@hotmail.com>
2024-01-15 15:50:38 -0700

    desplegable description

---
+++ ParkDetailsView.swift
@@ -0,0 +1,87 @@
+//
+//  ParkDetailsView.swift
+//  OutdoorNest
+//
+//  Created by Daniel Puente on 1/6/24.
+//
+
+import SwiftUI
+
+struct ParkDetailsView: View {
+    @State var parkDetailsData: [ParkDetailsData]
+    
+    var body: some View {
+        List {
+            ForEach(parkDetailsData, id: \.id) { item in
+                switch item.type {
+               
+                case .heroImage:
+                    Section(header: Text("Images").textCase(.none).font(.title2)) {
+                        if
+                            let heroImageData = item as? ParkDetailsImage,
+                            let url = URL(string: heroImageData.imageUrl) {
+                            AsyncImage(url: url) { image in
+                                image
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width:300, height: 300)
+                                    .cornerRadius(10)
+                                    .shadow(radius: 30)
+                            } placeholder: {
+                                Image(systemName: "tree.fill")
+                            }
+                        }
+                    }
+              
+                case .description:
+                    Section(header: Text("Descriptions").textCase(.none).font(.title2)) {
+                        if let descriptionData = item as? ParkDetailsDescription {
+                            Text(descriptionData.description)
+                        }
+                    }
+             
+                case .activities:
+                    Section(header: Text("Activities").textCase(.none).font(.title2)) {
+                        if let activitiesData = item as? ParkDetailsActivities {
+                            VStack {
+                                ForEach(activitiesData.acitivities, id: \.self) {
+                                    Text("\($0)")
+                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .foregroundStyle(Color.customColor.forestGreenColor)
+        
+    }
+    
+    static func generateListData(with park: Park? = nil) -> [ParkDetailsData] {
+        var parkDetailsData = [ParkDetailsData]()
+        
+        guard let park else { return parkDetailsData}
+        
+        if let imageUrl = park.images?.first?.url {
+            let heroImageData = ParkDetailsImage(imageUrl: imageUrl)
+            parkDetailsData.append(heroImageData)
+        }
+        
+        let descriptionData = ParkDetailsDescription(description: park.description)
+        parkDetailsData.append(descriptionData)
+        
+        if let activities = park.activities {
+            let activitiesData = ParkDetailsActivities(acitivities: activities.compactMap { $0.name })
+            parkDetailsData.append(activitiesData)
+        }
+        return parkDetailsData
+    }
+}
+
+struct ParkDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ParkDetailsView(parkDetailsData: ParkDetailsView.generateListData())
+    }
+}
+
